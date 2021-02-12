093ee771a4aaa5fe1056264198881397fed18e31
Randall Teves <rteves0914@gmail.com>
2021-02-12 03:24:44 -0800

    Created markers

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,46 +1,74 @@
-var myMap = L.map("map", {
-    center: [39.0997, -94.5786],
-    zoom: 8
-});
+// Store our API endpoint inside queryUrl
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-// Create the base tile layer
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+// Perform a GET request to the query URL
+d3.json(url, function(data) {
+  // Once we get a response, send the data.features object to the createFeatures function
+  createFeatures(data.features);
+
+
+// Create a new choropleth layer
+geojson = L.choropleth(data.features.geometry, {
+    valueProperty: "coordinates[2]",
+    scale: ["#8EF525", "#FA2234"],
+    steps: 6,
+    mode: "q",
+    style: {
+      color: "#fff",
+      weight: 1,
+      fillOpacity: 0.8
+    }};
+
+function createFeatures(earthquakeData) {
+
+  // Define a function we want to run once for each feature in the features array
+  // Give each feature a popup describing the place and time of the earthquake
+  function onEachFeature(feature, layer) {
+    layer.bindPopup("<h3>" + feature.properties.place +
+      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+  }
+
+  // Create a GeoJSON layer containing the features array on the earthquakeData object
+  // Run the onEachFeature function once for each piece of data in the array
+  var earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature
+  });
+
+  // Sending our earthquakes layer to the createMap function
+  createMap(earthquakes);
+}
+
+function createMap(earthquakes) {
+
+  // Define streetmap and darkmap layers
+  var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 18,
     zoomOffset: -1,
-    id: "satellite-v10",
+    id: "mapbox/streets-v11",
     accessToken: API_KEY
-}).addTo(myMap);
+  });
 
-//   // Initialize all of the LayerGroups we'll be using
-// var layers = {
-//     Tectonic_Plates: new L.LayerGroup(),
-//     Earthquakes: new L.LayerGroup()
-//   };
+  // Create overlay object to hold our overlay layer
+  var overlayMaps = {
+    Earthquakes: earthquakes
+  };
 
-  // URL that holds JSON data
-  var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
+  // Create our map, giving it the streetmap and earthquakes layers to display on load
+  var myMap = L.map("mapid", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 2,
+    layers: [streetmap, earthquakes]
+  });
 
-  // Use D3 to go through the JSON file, and strip out the latitude and longitude 
-  // of each earthquake and plot with a marker
-  d3.json(url, function(response) {
-
-    // Log the json data
-    console.log(response);
-
-    // Create an empty array that will hold the coordinates of earthquakes
-    var markers = [];
-
-    // For loop to gather all the coordinates and push onto the empty array of markers
-    for (var i = 0; i < response.length; i++) {
-
-        var location = response[i].location;
-
-        if (location) {
-            markers.push([location.coordinates[0], location.coordinates[1]]);
-        }
-  }
-   // Add the markers to the map   
-  L.marker(markers).addTo(myMap);
-});
+  // Create a layer control
+  // Pass in our baseMaps and overlayMaps
+  // Add the layer control to the map
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+}
+}
